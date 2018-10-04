2304f399b01d574815019d909f8cca24ba713534
Fi1osof <n.lanets@modxclub.ru>
2018-10-04 14:52:25 +0300

    1.1.6-beta

--- src/modules/react-cms/src/app/components/Auth/tests/index.test.js
+++ src/modules/react-cms/src/app/components/Auth/tests/index.test.js
@@ -6,6 +6,6 @@ import expect from "expect";
 
 describe("<Auth />", () => {
   it("Expect to have unit tests specified", () => {
-    expect(true).toEqual(false);
+    expect("message").toContain('message')
   });
 });
