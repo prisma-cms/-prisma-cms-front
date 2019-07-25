7c6248617e8be1e65aae778b81e7af7a5a6430d6
Fi1osof <n.lanets@modxclub.ru>
2019-07-25 07:32:55 +0300

    Added service-worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from 'react'
 
 import App from "./dev";
 
+import * as serviceWorker from './serviceWorker';
+
 // import "moment/locale/ru";
 
 // import * as queryFragments from "./schema/generated/api.fragments";
@@ -15,5 +17,10 @@ if (node) {
   // queryFragments={queryFragments}
   // lang="en"
   />, node);
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
 }
 
